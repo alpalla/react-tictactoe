67bb520f18904bb6f8e672211c331c699fc50ca4
Al <46252231+alpalla@users.noreply.github.com>
2020-03-29 19:48:08 +0200

    Updates

---
+++ precache-manifest.d42abd177b478c435d2e42ba9a0e0c50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb650ccf28664b27afb4e092f2b7cc7c",
+    "url": "/alpalla/react-tictactoe/index.html"
+  },
+  {
+    "revision": "9823137cb8498e0b538e",
+    "url": "/alpalla/react-tictactoe/static/css/main.48a3cf93.chunk.css"
+  },
+  {
+    "revision": "8e3577e6b3d69b48bb42",
+    "url": "/alpalla/react-tictactoe/static/js/2.db1b893b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/alpalla/react-tictactoe/static/js/2.db1b893b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9823137cb8498e0b538e",
+    "url": "/alpalla/react-tictactoe/static/js/main.4d2b63be.chunk.js"
+  },
+  {
+    "revision": "673458d03d4b7671a291",
+    "url": "/alpalla/react-tictactoe/static/js/runtime-main.802dc25a.js"
+  }
+]);
